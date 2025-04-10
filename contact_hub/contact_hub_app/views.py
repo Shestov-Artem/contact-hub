20cc4c351378980461a8ecd47ade7ef3ab1a813c
Артем Шестов <artoymsh75@gmail.com>
2025-04-10 23:47:45 +0300

    Изменено отображение ошибок формы

--- contact_hub/contact_hub_app/views.py
+++ contact_hub/contact_hub_app/views.py
@@ -9,19 +9,23 @@ def register_view(request):
     return HttpResponse("Страница регистрации (здесь будет форма позже)")
 
 def login_view(request):
+    error_message = None
+    
     if request.method == 'POST':
-        form = CustomLoginForm(request.POST)  # Убрали параметр `data=`
+        form = CustomLoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             
-            # Просто выводим данные в консоль для теста
-            print(f"Получены данные: Логин - {username}, Пароль - {password}")
-            
-            # Перенаправляем на главную страницу
-            return redirect('home')  # Или другую страницу
-            
+            # Кастомная проверка (замените на реальную проверку в БД позже)
+            if username != "1" and password != "1":
+                return redirect('home')
+            else:
+                error_message = "Неверный логин или пароль"
     else:
         form = CustomLoginForm()
     
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {
+        'form': form,
+        'error_message': error_message  # Передаём сообщение об ошибке
+    })
